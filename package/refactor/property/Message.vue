60a7ff56b31d4702536504bdc9f83713151792ad
xin2ber <42444880@qq.com>
2021-06-07 19:05:09 +0800

    任务监听器

--- package/refactor/property/Message.vue
+++ package/refactor/property/Message.vue
@@ -12,24 +12,48 @@
       </el-table-column>
     </el-table>
     <div class="element-drawer__button">
-      <el-button size="mini" type="primary" icon="el-icon-plus" @click="openListenerForm(null)">添加监听器</el-button>
+      <el-button size="mini" type="primary" icon="el-icon-plus" @click="openListenerForm(null)">添加</el-button>
     </div>
 
     <!-- 监听器 编辑/创建 部分 -->
     <el-dialog :visible.sync="listenerFormModelVisible" title="消息配置" width="600px" :close-on-click-modal="false" :close-on-press-escape="false">
       <el-form size="mini" :model="listenerForm" label-width="96px" ref="listenerFormRef" @submit.native.prevent>
-        <el-form-item label="消息描述" prop="remark" :rules="{ trigger: ['blur', 'change'] }">
+        <el-form-item label="消息描述" prop="remark" :rules="{required: true, message: '请输入', trigger: ['blur', 'change'] }">
           <el-input v-model="listenerForm.remark" clearable />
         </el-form-item>
-        <el-form-item label="执行时机" prop="event" :rules="{ required: true, trigger: ['blur', 'change'] }">
+        <el-form-item label="执行时机" prop="event" :rules="{ required: true, message: '请输入', trigger: ['blur', 'change'] }">
           <el-select v-model="listenerForm.event">
             <el-option v-for="i in Object.keys(listenerEventTypeObject)" :key="i" :label="listenerEventTypeObject[i]" :value="i" />
           </el-select>
         </el-form-item>
+        <el-form-item label="通知人" prop="recipients" >
+          <el-tag
+            :key="tag"
+            v-for="tag in listenerForm.recipients"
+            closable
+            :disable-transitions="false"
+            @close="handleClose(tag)">
+            {{recipientTypes[tag] || tag}}
+          </el-tag>
+          <el-select
+            filterable
+            allow-create
+            class="input-new-tag"
+            v-if="inputVisible"
+            v-model="inputValue"
+            ref="saveTagInput"
+            size="small"
+            @keyup.enter.native="handleInputConfirm"
+            @change="handleInputConfirm"
+          >
+            <el-option v-for="i in selectableRecipients" :key="i" :label="recipientTypes[i]" :value="i" />
+          </el-select>
+          <el-button v-else class="button-new-tag" size="small" @click="showInput">+ 添加</el-button>
+        </el-form-item>
         <el-form-item label="条件" prop="condition" >
           <el-input v-model="listenerForm.condition" clearable />
         </el-form-item>
-        <el-form-item label="消息模板" prop="template" :rules="{ required: true, trigger: ['blur', 'change'] }">
+        <el-form-item label="消息模板" prop="template" :rules="{ required: true, message: '请输入', trigger: ['blur', 'change'] }">
           <el-input type="textarea" v-model="listenerForm.template" size="mini" resize="vertical" :autosize="{ minRows: 3, maxRows: 5 }"/>
         </el-form-item>
         <el-form-item label="消息类型" :rules="{ required: true }">
@@ -61,29 +85,61 @@ export default {
       elementListenersList: [],
       listenerEventTypeObject: {create: '创建',end: '完成'},
       listenerFormModelVisible: false,
-      listenerForm: { checkedMessageTypes: ["ip"]},
+      listenerForm: { checkedMessageTypes: ["mail"]},
       editingListenerIndex: -1, // 监听器所在下标，-1 为新增
-      messageTypes: [{key:"sms",name:"短信"},{key:"ip",name:"云之家"}]
+      messageTypes: [{key:"sms",name:"短信"},{key:"mail",name:"邮件"}],
+      recipientTypes:  {candidates: '任务候选人',start: '流程发起人'},
+      inputVisible: false,
+      inputValue: ''
     };
   },
   mounted() {
     this.resetListenersList();
   },
+  computed: {
+    selectableRecipients() {
+      let selectableRecipients = []
+      console.log(this.listenerForm.recipients);
+      for(let key in this.recipientTypes) {
+        //当前不存在，就放到可选择中
+        if (this.listenerForm.recipients.indexOf(key) < 0) {
+            selectableRecipients.push(key);
+        }
+      }
+      console.log(selectableRecipients);
+      return selectableRecipients;
+    }
+  },
   methods: {
+    handleClose(tag) {
+      this.listenerForm.recipients.splice( this.listenerForm.recipients.indexOf(tag), 1);
+    },
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick(_ => {
+        console.log(this.$refs.saveTagInput);
+        this.$refs.saveTagInput.$refs.reference.focus();
+      });
+    },
+    handleInputConfirm() {
+      let inputValue = this.inputValue;
+      if (inputValue) {
+        this.listenerForm.recipients.push(inputValue);
+      }
+      this.inputVisible = false;
+      this.inputValue = '';
+    },
     resetListenersList() {
       this.otherExtensionList = [];
       this.elementListeners =
         this.element.businessObject?.extensionElements?.values?.filter(ex => {
-          console.log(ex)
-          if (ex.$type !== `flowable:TaskListener` || !ex.delegateExpression || ex.delegateExpression !== '${customTaskListener}') {
+          if (ex.$type !== `flowable:TaskListener` || ex.delegateExpression !== '${customTaskListener}') {
             this.otherExtensionList.push(ex);
             return false
           }
           return true;
         }) ?? [];
-      console.log(this.elementListeners)
       this.elementListenersList = this.elementListeners.map(listener => this.initListenerType(listener));
-      console.log(this.elementListenersList)
     },
     initListenerType(item) {
       let listenerForm = {
@@ -92,6 +148,9 @@ export default {
       item.fields.forEach(field => {
         if(field.name === 'remark') {
           listenerForm.remark = field.expression
+        }
+        if(field.name === 'recipients') {
+          listenerForm.recipients = field.expression.split(',')
         }
         if(field.name === 'condition') {
           listenerForm.condition = field.expression
@@ -110,7 +169,7 @@ export default {
         this.listenerForm = listener;
         this.editingListenerIndex = index;
       } else {
-        this.listenerForm = {checkedMessageTypes: ["ip"]};
+        this.listenerForm = {checkedMessageTypes: ["mail"],recipients:["candidates"]};
         this.editingListenerIndex = -1; // 标记为新增
       }
       // 打开侧边栏并清楚验证状态
@@ -137,7 +196,6 @@ export default {
     },
     // 保存监听器
     async saveListenerConfig() {
-      console.log(1212)
       let validateStatus = await this.$refs["listenerFormRef"].validate();
       if (!validateStatus) return; 
       const listenerObject = this.createListner()
@@ -148,13 +206,8 @@ export default {
         this.elementListeners.splice(this.editingListenerIndex, 1, listenerObject);
         this.elementListenersList.splice(this.editingListenerIndex, 1, this.listenerForm);
       }
-      console.log(listenerObject)
-      console.log(this.elementListeners)
-      console.log(this.elementListenersList)
-      console.log(this.otherExtensionList.concat(this.elementListeners))
       let extensionElements = this.modeler.get('moddle').create('bpmn:ExtensionElements')
       extensionElements.values = this.otherExtensionList.concat(this.elementListeners)
-      console.log(extensionElements)
       this.updateProperties({ extensionElements: extensionElements })
       this.listenerFormModelVisible = false;
     },
@@ -175,10 +228,30 @@ export default {
       listener['event'] = this.listenerForm.event
       listener['delegateExpression'] = '${customTaskListener}'
       for(let key in this.listenerForm) {
-        listener.get('fields').push(this.createField(key))
+        if(key !== 'event') {
+          listener.get('fields').push(this.createField(key))
+        }
       }
       return listener
     }
   }
 };
 </script>
+<style scoped>
+  .el-tag + .el-tag {
+    margin-left: 10px;
+  }
+  .button-new-tag {
+    width: 150px;
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .input-new-tag {
+    width: 150px;
+    margin-left: 10px;
+    vertical-align: bottom;
+  }
+</style>
